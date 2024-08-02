914b0db2feabf23ec7c90d38455e439309230a37
JHONE JOSE MARTINS DE OLIVEIRA SILVA <20211044110027@ifrn.local>
2024-08-01 21:41:20 -0300

    upgrade

--- resources/views/curso-listagem.blade.php
+++ resources/views/curso-listagem.blade.php
@@ -1,32 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container">
-        <table class="table table-warning table-striped">
-            <tr>
-                <th>Nome</th>
-                <th>Idade</th>
-            </tr>
+@extends('app')
+
+@section('conteudo')
+<table class="table table-warning table-striped">
+    <tr>
+        <th>Nome</th>
+        <th>Idade</th>
+    </tr>
         
-            @foreach($objetos as $objeto)
-            <tr>
-                <td> {{ $objeto['nome'] }} </td>
-                <td> {{ $objeto['idade'] }} </td>
-            </tr>
-            @endforeach
-        </table>
-    </div>
-
-    <h1>Olá Mundo!</h1>
-    <h1>Hello World!</h1>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+    @foreach($objetos as $objeto)
+    <tr>
+        <td> {{ $objeto['nome'] }} </td>
+        <td> {{ $objeto['idade'] }} </td>
+    </tr>
+    @endforeach
+</table>
+@endsection('conteudo')
