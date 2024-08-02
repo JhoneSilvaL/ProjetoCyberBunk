a9a1c58f5eadcccfe2f987462f56021da52b9332
JHONE JOSE MARTINS DE OLIVEIRA SILVA <20211044110027@ifrn.local>
2024-08-02 20:01:56 -0300

    aula 02/08

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,10 @@ use App\Http\Controllers\CursoController;
 Route::get('/', [CursoController::class, 'index']);
 
 Route::get('/curso-listagem', [CursoController::class, 'index']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/welcome', function(){
+    return view("welcome");
+});
