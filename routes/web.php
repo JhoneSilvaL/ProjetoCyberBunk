8a508379fc756083ae2cea779e205e0e015552c9
JHONE JOSE MARTINS DE OLIVEIRA SILVA <20211044110027@ifrn.local>
2024-08-09 20:25:29 -0300

    update 3

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,8 @@ use App\Http\Controllers\CursoController;
 
 Route::get('/', [CursoController::class, 'index']);
 
-Route::get('/curso-listagem', [CursoController::class, 'index']);
+Route::get('/curso-listagem', [CursoController::class, 'index'])->name('curso.listagem');
+Route::get('/curso-cadastro', [CursoController::class, 'create'])->name('curso.cadastro');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
