bdd6d3a901509bdc286938eefc6dde0639d234d8
JHONE JOSE MARTINS DE OLIVEIRA SILVA <20211044110027@ifrn.local>
2024-08-23 19:47:29 -0300

    update 3.0

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,15 @@ use App\Http\Controllers\CursoController;
 
 Route::get('/', [CursoController::class, 'index']);
 
-// Route, verbo, nome da rota temporário, classe do controller utilizada, nome da rota permanente.
-Route::get('/curso-listagem', [CursoController::class, 'index'])->name('curso.listagem');
-Route::get('/curso-cadastro', [CursoController::class, 'create'])->name('curso.cadastro');
-Route::post('/curso-salvar', [CursoController::class, 'store'])->name('curso.salvar');
-Route::get('/curso-alterar/{id}', [CursoController::class, 'edit'])->name('curso.alterar');
-Route::put('/curso-atualizar/{id}', [CursoController::class, 'update'])->name('curso.atualizar');
-Route::get('/curso-deletar/{id}', [CursoController::class, 'destroy'])->name('curso.deletar');
+Route::middleware('auth')->group(function(){
+    // Route, verbo, nome da rota temporário, classe do controller utilizada, nome da rota permanente.
+    Route::get('/curso-listagem', [CursoController::class, 'index'])->name('curso.listagem');
+    Route::get('/curso-cadastro', [CursoController::class, 'create'])->name('curso.cadastro');
+    Route::post('/curso-salvar', [CursoController::class, 'store'])->name('curso.salvar');
+    Route::get('/curso-alterar/{id}', [CursoController::class, 'edit'])->name('curso.alterar');
+    Route::put('/curso-atualizar/{id}', [CursoController::class, 'update'])->name('curso.atualizar');
+    Route::get('/curso-deletar/{id}', [CursoController::class, 'destroy'])->name('curso.deletar');
+
+});
+
+Auth::routes();
